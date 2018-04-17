a6e776334df34a86718403e6e46484e935f96db1
Ilana Nagib <ilana.nagib@yahoo.com>
2018-04-17 11:52:38 -0500

    commit

--- server.js
+++ server.js
@@ -3,4 +3,18 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.listen(PORT);
+// req is request and res is response
+app.get('/', function (req, res) {
+    res.send('Hello World');
+})
+
+app.get('/Hey', function (req, res) {
+    res.send('Howdy');
+})
+
+app.listen(PORT, function () {
+    console.log(`listening on port: ${PORT}`)
+});
+
+// to fix code: option + shift + f
+
